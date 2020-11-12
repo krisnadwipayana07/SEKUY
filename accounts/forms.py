5306026985f9f4b050a180fdee7bcc5085eda81d
Boomski99 <krisnadwipayana30@gmail.com>
2020-11-13 00:25:16 +0800

    terbaru yee

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth import authenticate, get_user_model
 
 User = get_user_model()
 
-
 class userLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class Materi(models.Model):
-    # first_name = models.CharField("Judul Materi : ",max_length=30)
-    comment = models.CharField("isi Materi",max_length=3000)
+class isiMateri(models.Manager):
+    def buatMateri(self, judul, comment, *args , **kwargs):
+        materi = self.create(judul=judul,comment=comment)
+        return materi
 
+class Materi(models.Model):
+    judul = models.CharField(max_length=30)
+    comment = models.CharField(max_length=3000)
+
+    if judul and command :
+        objects = isiMateri()
+
+materi = Materi.objects.buatMateri("Judul Materi","Isi Materi")
